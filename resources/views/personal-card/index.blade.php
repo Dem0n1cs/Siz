7ec81b025874a53c2e0b611d179bbec0832e0acb
Дмитрий Осмоловский <osmolovskiidimon@mail.ru>
2023-10-29 12:18:59 +0300

    Pre-alpha version 0.1.4 Фикс отображения личных карточек

--- resources/views/personal-card/index.blade.php
+++ resources/views/personal-card/index.blade.php
@@ -6,9 +6,9 @@
                 {{ session()->get('success') }}
             </div><br/>
         @endif
-  {{--      <div class="pull-right mb-2">
-            <a class="btn btn-success" href="{{ route('personal_card.create') }}">Добавить</a>
-        </div>--}}
+        {{--      <div class="pull-right mb-2">
+                  <a class="btn btn-success" href="{{ route('personal_card.create') }}">Добавить</a>
+              </div>--}}
         <table class="table">
             <thead>
             <tr class="table-warning">
@@ -23,26 +23,28 @@
 
             @foreach($personalCards as $branch)
                 <tr>
-                    <td data-name="branch" data-id="{{$branch->id}}" rowspan="">{{$branch->title}}</td>
+                    <td data-name="branch" data-branch_id="{{$branch->id}}" rowspan="">{{$branch->title}}</td>
                 </tr>
                 @foreach($branch->departments as $department)
                     <tr>
-                        <td data-name="department" data-id="{{$branch->id}}" rowspan="">{{$department->title}}</td>
+                        <td data-name="department" data-branch_id="{{$branch->id}}" data-department_id="{{$department->id}}" rowspan="">{{$department->title}}</td>
                     </tr>
                     @foreach($department->divisions as $division)
                         <tr>
-                            <td data-name="division" data-id="{{$branch->id}}" rowspan="">{{$division->full_title}}</td>
+                            <td data-name="division" data-branch_id="{{$branch->id}}" data-department_id="{{$department->id}}" data-division_id="{{$division->id}}" rowspan="">{{$division->full_title}}</td>
                         </tr>
                         @foreach($division->user as $user)
                             <tr>
                                 <td>
                                     {{$user->full_name}}
                                 </td>
-                                <td data-name="user" data-id="{{$branch->id}}">
+                                <td data-name="user" data-branch_id="{{$branch->id}}" data-department_id="{{$department->id}}" data-division_id="{{$division->id}}">
                                     @if(!$user->personalcard)
-                                    <a href="{{ route('personal_card.create', $user->id) }}" class="btn btn-info btn-sm">Создать</a>
+                                        <a href="{{ route('personal_card.create', $user->id) }}"
+                                           class="btn btn-info btn-sm">Создать</a>
                                     @else
-                                        <a href="{{ route('personal_card.edit', $user->personalcard->id) }}" class="btn btn-info btn-sm">Редактировать</a>
+                                        <a href="{{ route('personal_card.edit', $user->personalcard->id) }}"
+                                           class="btn btn-info btn-sm">Редактировать</a>
                                     @endif
                                 </td>
                             </tr>
@@ -55,13 +57,29 @@
         <div>
             <script type="module">
                 $(document).ready(function () {
-                    $('.table tbody>tr').children('td[data-name="branch"]').each(function () {
-                        const id = $(this).data('id');
-                        const countUser = parseInt($('.table tbody>tr').children(`td[data-name="user"][data-id=${id}]`).length)
-                        $(`td[data-name="branch"][data-id=${id}]`).prop('rowspan', countUser + 3)
-                        $(`td[data-name="department"][data-id=${id}]`).prop('rowspan', countUser + 2)
-                        $(`td[data-name="division"][data-id=${id}]`).prop('rowspan', countUser + 1)
-                    })
+                    const tableTbodyTr = $('.table tbody>tr');
+
+                    function updateRowspan(elementName) {
+                        tableTbodyTr.children(`td[data-name="${elementName}"]`).each(function () {
+                            const elementId = $(this).data(`${elementName}_id`);
+                            const countUser = tableTbodyTr.children(`td[data-name="user"][data-${elementName}_id="${elementId}"]`).length;
+                            let rowspanElement = countUser + 1;
+
+                            if (elementName !== 'division') {
+                                const countDivision = tableTbodyTr.children(`td[data-name="division"][data-${elementName}_id="${elementId}"]`).length;
+                                rowspanElement += countDivision;
+                            }
+
+                            if (elementName === 'branch') {
+                                const countDepartment = tableTbodyTr.children(`td[data-name="department"][data-branch_id="${elementId}"]`).length;
+                                rowspanElement += countDepartment;
+                            }
+
+                            $(this).attr('rowspan', rowspanElement);
+                        });
+                    }
+
+                    ['branch', 'department', 'division'].forEach(updateRowspan);
                 });
             </script>
 @endsection
